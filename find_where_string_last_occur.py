a94a8a877026ad684a91e005f304d96c7ca3ea7d
Erica Guillermo <ericajhaneg@gmail.com>
2025-04-01 14:51:23 +0800

    Create find_where_string_last_occur.py

---
+++ find_where_string_last_occur.py
@@ -0,0 +1,11 @@
+words = input("Enter a word: ")
+string = input("Enter substring to find last index: ")
+sub_len = len(string)
+found = False
+for me in range(len(words) - sub_len, -1, -1):
+    if words[me:me + sub_len] == string:
+        print(me)
+        found = True
+        break
+if not found:
+    print("string not found")
